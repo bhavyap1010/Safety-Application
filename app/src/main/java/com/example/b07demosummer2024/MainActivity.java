6ea5592bfb345f8d0e9f047b090ce0c54451e88e
mlee4681216 <mich481216@gmail.com>
2025-07-22 22:36:39 -0400

    Added privacy notice which pops up at startup

--- app/src/main/java/com/example/b07demosummer2024/MainActivity.java
+++ app/src/main/java/com/example/b07demosummer2024/MainActivity.java
@@ -17,15 +17,30 @@ import android.content.Intent;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import android.content.SharedPreferences;
+import androidx.appcompat.app.AlertDialog;
+
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase db;
+    private static final String PREFS_NAME = "prefs";
+    private static final String KEY_PRIVACY_AGREED = "privacy_agreed";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*To always have the privacy notice, uncomment the code below.
+        * Otherwise, the privacy notice only pops up the first time someone
+        * runs the app.*/
+        /*
+        getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
+                .edit()
+                .remove(KEY_PRIVACY_AGREED)
+                .apply();
+        */
+        showPrivacyDialogIfNeeded();
         db = FirebaseDatabase.getInstance("https://b07-demo-summer-2024-default-rtdb.firebaseio.com/");
         DatabaseReference myRef = db.getReference("testDemo");
 
@@ -50,6 +65,27 @@ public class MainActivity extends AppCompatActivity {
             getSupportFragmentManager().popBackStack();
         } else {
             super.onBackPressed();
+        }
+    }
+
+    private void showPrivacyDialogIfNeeded() {
+        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean agreed = prefs.getBoolean(KEY_PRIVACY_AGREED, false);
+
+        if (!agreed) {
+            new AlertDialog.Builder(this)
+                    .setTitle(getString(R.string.privacy_notice_title))
+                    .setMessage(getString(R.string.privacy_notice_msg))
+                    .setCancelable(false)
+                    .setPositiveButton(R.string.agree, (d, w) -> {
+                        prefs.edit().putBoolean(KEY_PRIVACY_AGREED, true).apply();
+                        // user continues into the app
+                    })
+                    .setNegativeButton(R.string.disagree, (d, w) -> {
+                        // app exits
+                        finishAffinity();
+                    })
+                    .show();
         }
     }
 }
